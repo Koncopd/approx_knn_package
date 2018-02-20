b92766d5cab8615f258569eb45a8d72c5b152788
Sergei Rybakov <rybakov@in.tum.de>
2018-02-20 14:05:28 +0100

    indent

--- approx_knn_c/approx_knn.cpp
+++ approx_knn_c/approx_knn.cpp
@@ -8,39 +8,39 @@
 extern "C"
 {
 
-#ifdef _WIN32
+  #ifdef _WIN32
   __declspec(dllexport)
-#endif
+  #endif
 
-void get_distances_and_neighbors(double* X, int N, int D, int* knn, double* dists, int K, int num_trees, int search_k) {
+  void get_distances_and_neighbors(double* X, int N, int D, int* knn, double* dists, int K, int num_trees, int search_k) {
 
-		AnnoyIndex<int, double, Euclidean, Kiss32Random> tree = AnnoyIndex<int, double, Euclidean, Kiss32Random>(D);
+    AnnoyIndex<int, double, Euclidean, Kiss32Random> tree = AnnoyIndex<int, double, Euclidean, Kiss32Random>(D);
 
     for(int i=0; i<N; ++i){
       double *vec = new double[D];
 
       for(int z=0; z<D; ++z){
         vec[z] = X[i*D+z];
-			}
-			tree.add_item(i, vec);
+      }
+      tree.add_item(i, vec);
 
       delete[] vec;
-		}
-		tree.build(num_trees);
+    }
+    tree.build(num_trees);
 
-		//Check if it returns enough neighbors
-		std::vector<int> closest;
-		std::vector<double> closest_distances;
-		for (int n = 0; n < 100; n++){
-			tree.get_nns_by_item(n, K+1, search_k, &closest, &closest_distances);
-			unsigned int neighbors_count = closest.size();
-			if (neighbors_count < K+1 ) {
+    //Check if it returns enough neighbors
+    std::vector<int> closest;
+    std::vector<double> closest_distances;
+    for (int n = 0; n < 100; n++){
+      tree.get_nns_by_item(n, K+1, search_k, &closest, &closest_distances);
+      unsigned int neighbors_count = closest.size();
+      if (neighbors_count < K+1 ) {
         std::cout<<"Requesting "<<K<<" neighbors, but ANNOY is only giving us "<<neighbors_count<<". Please increase search_k"<<std::endl;
-				return;
-			}
-		}
+        return;
+      }
+    }
 
-		const size_t nthreads = std::thread::hardware_concurrency();
+    const size_t nthreads = std::thread::hardware_concurrency();
 
     std::cout<<"Parallel ("<<nthreads<<" threads)"<<std::endl;
     std::vector<std::thread> threads(nthreads);
@@ -71,6 +71,6 @@ void get_distances_and_neighbors(double* X, int N, int D, int* knn, double* dist
       }
       std::for_each(threads.begin(),threads.end(),[](std::thread& x){x.join();});
 
-	}
+    }
 
-}
+  }
